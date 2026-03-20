3d9c655e9300d05696c23190efc08e68cc21a200
Frengocode <wendypython17@gmail.com>
2026-03-20 13:35:46 +0500

    [ADDED]

--- src/common/shared/auth/interfaces/token/token_generator.py
+++ src/common/shared/auth/interfaces/token/token_generator.py
@@ -1,7 +1,7 @@
 from typing import Protocol
-
+from src.common.shared.auth.interfaces.token.sub import Sub
 
 class ITokenGenerator(Protocol):
     """Generates tokens like jwt, token, maybe special custom token"""
 
-    def generator(sub: int) -> str: ...
+    def generator(sub: Sub) -> str: ...
