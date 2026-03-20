f8ae8bef3ae2cea819ce517529640d7872aa04c4
Frengocode <wendypython17@gmail.com>
2026-03-20 13:30:49 +0500

    [Refactored]

---
+++ src/common/shared/auth/infrastructure/hash/bcrypt_hash.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+from src.common.shared.auth.interfaces.hash.hash import IHash
+
+
+class BcryptHash(IHash):
+    def __init__(self, context: CryptContext) -> None:
+        self.context = context
+
+    def hash_value(self, value: str) -> str:
+        return self.context.hash(value)
+
+    def verify_plain_value(self, plain_value: str, hash: str) -> bool:
+        isMatchs: bool = self.context.verify(plain_value, hash)
+        return isMatchs
