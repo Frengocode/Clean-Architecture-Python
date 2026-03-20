08ba09f5175ee6b97166c35c7bf45fc5d7878bab
Frengocode <wendypython17@gmail.com>
2026-03-20 13:14:09 +0500

    [Added] Delete Update use cases

---
+++ src/common/shared/auth/interfaces/token/token_generator.py
@@ -0,0 +1,6 @@
+from typing import Protocol
+
+
+class ITokenGenerator(Protocol):
+    """ Generates tokens like jwt, token, maybe special custom token """
+    async def generator(sub: int) -> str: ...
