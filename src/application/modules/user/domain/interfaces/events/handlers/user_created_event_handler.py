b97429c3b8da81ba1281decfbb6e62aa17229175
Frengocode <wendypython17@gmail.com>
2026-03-22 01:16:20 +0500

    [ADDED]

---
+++ src/application/modules/user/domain/interfaces/events/handlers/user_created_event_handler.py
@@ -0,0 +1,11 @@
+from typing import Protocol
+from src.application.modules.user.domain.interfaces.events.payloads.user_created_event_payload import (
+    UserCreatedEventPayload,
+)
+
+
+class IUserCreatedEventHandler(Protocol):
+
+    async def handle(self, payload: UserCreatedEventPayload) -> None: ...
+
+    """ Handles user created event """
