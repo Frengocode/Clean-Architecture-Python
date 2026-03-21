6dd1fe8f044adaad9cded0a071c2718850c6d6d3
Frengocode <wendypython17@gmail.com>
2026-03-21 21:34:05 +0500

    [ADDED]

--- src/application/modules/user/domain/entities/user.py
+++ src/application/modules/user/domain/entities/user.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from src.application.modules.user.domain.value_objects.user_id import UserId
 from src.application.modules.user.domain.value_objects.email import Email
 from src.application.modules.user.domain.value_objects.username import Username
+from src.application.modules.user.domain.value_objects.password import Password
 
 
 @dataclass(frozen=True, slots=True)
@@ -9,7 +10,13 @@ class User:
     id: UserId
     email: Email
     username: Username
+    password: Password
 
     def exist_user(self, email: Email) -> None:
         if self.email.value == email:
             raise Exception("User already exists")
+
+    def update_password(self, old_pasword: str, new_password: str) -> str:
+        if self.password != old_pasword:
+            raise Exception("InCorrect Password")
+        self.password = new_password
