5224e2c8c0c550d61de6e46e434e2eeb863369c6
Frengocode <wendypython17@gmail.com>
2026-03-21 23:08:10 +0500

    [ADDED] c

---
+++ src/application/modules/user/domain/interfaces/repository/user_repository.py
@@ -0,0 +1,31 @@
+from src.application.modules.user.domain.entities.user import User
+from src.application.modules.user.domain.value_objects.user_id import UserId
+from src.application.modules.user.domain.value_objects.username import Username
+from src.application.modules.user.domain.value_objects.email import Email
+from src.application.modules.user.dto.requests.get_users_request import GetUsersRequest
+from typing import Any, List
+from abc import ABC, abstractmethod
+
+
+class IUserRepository(ABC):
+    """User repository abstraction"""
+
+    @abstractmethod
+    async def create_user(self, user: User) -> User: ...
+
+    @abstractmethod
+    async def __get_user(self, **filters: Any) -> User | None: ...
+
+    """ __get_user is a prviate method, uses at -> get_user_by_id, get_user_by_username, get_user_by_email  """
+
+    @abstractmethod
+    async def get_user_by_id(self, userId: UserId) -> User | None: ...
+
+    @abstractmethod
+    async def get_user_by_username(self, username: Username) -> User | None: ...
+
+    @abstractmethod
+    async def get_user_by_email(self, email: Email) -> User | None: ...
+
+    @abstractmethod
+    async def get_users(self, request: GetUsersRequest) -> List[User]: ...
---
+++ src/application/modules/user/dto/requests/get_users_request.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+from src.application.common.shared.pagination.base_pagination import BasePagination
+
+
+class GetUsersRequest(BaseModel, BasePagination):
+    pass
